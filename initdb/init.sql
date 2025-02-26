d7eb82f676cc7dd64a5cef594ba6a6c0771299e6
caiopeterlini <cmunhozpeterlini@gmail.com>
2025-02-25 22:31:26 -0300

    ajuste de organizacao e nomenclatura de tabelas; add modelagem de dados parcial;

--- initdb/init.sql
+++ initdb/init.sql
@@ -3,36 +3,34 @@ USE mydb;
 
 
 
-CREATE TABLE client(
-    client_id int NOT NULL  AUTO_INCREMENT,
-    cpf  varchar(11) NOT NULL ,  
-    client_name varchar(250) NOT NULL ,
-    PRIMARY KEY (client_id)
+CREATE TABLE client (
+    cli_id int NOT NULL AUTO_INCREMENT,
+    cli_cpf varchar(11) NOT NULL,
+    cli_name varchar(250) NOT NULL,
+    PRIMARY KEY (cli_id)
 );
 
-CREATE TABLE product(
-    product_id int  NOT NULL AUTO_INCREMENT,
-    product_name varchar(11) NOT NULL,  
-    product_price varchar(250) NOT NULL,
-    product_version int ,
-    PRIMARY KEY (product_id)
+CREATE TABLE product (
+    id int NOT NULL AUTO_INCREMENT,
+    p_name varchar(250) NOT NULL,
+    price decimal(10,2) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-
-CREATE TABLE order_delivery(
-    order_id int NOT NULL AUTO_INCREMENT,
-    order_cpf_id varchar(11) NOT NULL,  
-    order_total_price varchar(250) ,
-    PRIMARY KEY (order_id),
-    FOREIGN KEY (order_id) REFERENCES client (client_id) 
+CREATE TABLE orders (
+    id int NOT NULL AUTO_INCREMENT,
+    cpf_id int NOT NULL,
+    total_price decimal(10,2) NOT NULL,
+    FOREIGN KEY (cpf_id) REFERENCES client (cli_id),
+    PRIMARY KEY (id)
 );
 
-
-CREATE TABLE order_product(
-    order_product_id int NOT NULL AUTO_INCREMENT,
-    order_product_order_id int   NOT NULL,  
-    order_product_product_id int NOT NULL,
-    FOREIGN KEY (order_product_order_id) REFERENCES order_delivery (order_id), 
-    FOREIGN KEY (order_product_product_id) REFERENCES product (product_id),
-    PRIMARY KEY (order_product_id)
+CREATE TABLE item_order (
+    id int NOT NULL AUTO_INCREMENT,
+    order_id int NOT NULL,
+    product_id int NOT NULL,
+    quantity int NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders (id),
+    FOREIGN KEY (product_id) REFERENCES product (id),
+    PRIMARY KEY (id)
 );
